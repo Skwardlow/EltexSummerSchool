99eea6fed6e3546eac222d10a63c7b6b1dfb2b61
Skwardlow <gorskovdmitry@gmail.com>
2019-07-06 18:52:40 +0700

    user is described now

--- ClassNHomeworks/Lesson2/src/main/java/ru/skwardlow/User.java
+++ ClassNHomeworks/Lesson2/src/main/java/ru/skwardlow/User.java
@@ -1,4 +1,57 @@
 package ru.skwardlow;
 
-public class User {
+public abstract class User {
+    private String fio;
+    private String phone;
+    private String mailbox;
+    private Byte userid;
+
+    public void setUserid(Byte userid) {
+        this.userid = userid;
+    }
+
+    public Byte getUserid() {
+        return userid;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setMailbox(String mailbox) {
+        this.mailbox = mailbox;
+    }
+
+    public String getMailbox() {
+        return mailbox;
+    }
+
+    public User() {
+
+    }
+
+    public  User(byte userid, String fio, String phone, String mailbox) {
+        this.userid = userid;
+        this.fio =fio;
+        this.phone = phone;
+        this.mailbox = mailbox;
+    }
+
+
+
+
+
+
 }
