492e02e6cc70a7a0c78823c239a1d082d034f849
Skwardlow <gorskovdmitry@gmail.com>
2019-07-16 02:38:46 +0700

    owerwrited

--- ClassNHomeworks/Lesson4/JSON/JSONtest/src/main/java/ru/skwardlow/App.java
+++ ClassNHomeworks/Lesson4/JSON/JSONtest/src/main/java/ru/skwardlow/App.java
@@ -2,16 +2,38 @@ package ru.skwardlow;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App implements Json
 {
     public static void main ( String[] args )
     {
         System.out.println( "Hello World!" );
         ObjectMapper om = new ObjectMapper();
+        SimpleObject simpleObjectw = new SimpleObject("John",123,123.456);
+        try {
+            om.writeValue(new File("jack.json"),simpleObjectw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
+    @Override
+    public String toJSON() {
+
+        return null;
+    }
+
+    @Override
+    public void fromJSON(String str) {
+
+    }
 }
---
+++ ClassNHomeworks/Lesson4/JSON/JSONtest/src/main/java/ru/skwardlow/SimpleObject.java
@@ -0,0 +1,43 @@
+package ru.skwardlow;
+
+public class SimpleObject {
+    private String objectName;
+    private Integer objectCounter;
+    private Double isThatTrue;
+
+    SimpleObject(){
+        this.objectName = null;
+        this.objectCounter = null;
+        this.isThatTrue = null;
+    }
+
+    SimpleObject(String objectName,Integer objectCounter,Double isThatTrue){
+        this.objectName = objectName;
+        this.objectCounter = objectCounter;
+        this.isThatTrue = isThatTrue;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(String objectName) {
+        this.objectName = objectName;
+    }
+
+    public Integer getObjectCounter() {
+        return objectCounter;
+    }
+
+    public void setObjectCounter(Integer objectCounter) {
+        this.objectCounter = objectCounter;
+    }
+
+    public Double getIsThatTrue() {
+        return isThatTrue;
+    }
+
+    public void setIsThatTrue(Double isThatTrue) {
+        this.isThatTrue = isThatTrue;
+    }
+}
