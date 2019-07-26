dfde4a51557e410f1e2920ce3b8744962099cd55
Skwardlow <gorskovdmitry@gmail.com>
2019-07-26 19:51:18 +0700

    [MavenMC] useful files added

--- MavenMCdir/MavenMC2test/service/src/main/java/ru/skwardlow/sevice/Main.java
+++ MavenMCdir/MavenMC2test/service/src/main/java/ru/skwardlow/sevice/Main.java
@@ -1,4 +1,15 @@
 package ru.skwardlow.sevice;
 
+import ru.skwardlow.utils.Man;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello sevice");
+
+
+        Man man = new Man();
+        man.setName("Dmitry");
+        System.out.println(man.getName());
+
+    }
 }
--- MavenMCdir/MavenMC2test/utils/src/main/java/ru/skwardlow/utils/Man.java
+++ MavenMCdir/MavenMC2test/utils/src/main/java/ru/skwardlow/utils/Man.java
@@ -1,4 +1,22 @@
 package ru.skwardlow.utils;
 
 public class Man {
+    public static String TEST_HELLO="lalalah";
+    private String name;
+
+    public static String getTestHello() {
+        return TEST_HELLO;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void setTestHello(String testHello) {
+        TEST_HELLO = testHello;
+    }
 }
