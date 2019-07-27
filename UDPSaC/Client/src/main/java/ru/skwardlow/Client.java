e0bc7c069c5fc3141d6e3c5e46ec187ddf8ccfee
Skwardlow <gorskovdmitry@gmail.com>
2019-07-27 20:27:08 +0700

    [UDPSaC] added some bugs

--- UDPSaC/Client/src/main/java/ru/skwardlow/Client.java
+++ UDPSaC/Client/src/main/java/ru/skwardlow/Client.java
@@ -1,13 +1,18 @@
-package ru.Skwardlow;
+package ru.skwardlow;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        byte[] data = "Hello".getBytes();
+        System.out.println("Type what you want send to server ");
+        Scanner scanner = new Scanner(System.in);
+        String dataToSend = scanner.nextLine();
+        dataToSend.concat("\0");
+        byte[] data = dataToSend.getBytes();
         InetAddress addr = InetAddress.getByName("127.0.0.1");
         DatagramPacket pack =
                 new DatagramPacket(data, data.length, addr, 5678);
