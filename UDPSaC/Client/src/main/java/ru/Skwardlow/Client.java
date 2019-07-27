62fb19d12f38c58cae67bb57261ffbdc59b715b5
Skwardlow <gorskovdmitry@gmail.com>
2019-07-27 19:45:57 +0700

    [UDPSaC] Client added

--- UDPSaC/Client/src/main/java/ru/Skwardlow/Client.java
+++ UDPSaC/Client/src/main/java/ru/Skwardlow/Client.java
@@ -1,4 +1,18 @@
 package ru.Skwardlow;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        byte[] data = "Hello".getBytes();
+        InetAddress addr = InetAddress.getByName("127.0.0.1");
+        DatagramPacket pack =
+                new DatagramPacket(data, data.length, addr, 5678);
+        DatagramSocket ds = new DatagramSocket();
+        ds.send(pack);
+        ds.close();
+    }
 }
