9b47741f37bfabef6c18cdda4bbe46e839f0c65e
Skwardlow <gorskovdmitry@gmail.com>
2019-08-19 17:51:58 +0700

    [Reflection] init

--- Reflection/src/main/java/ru/skwardlow/reflection/Reflection.java
+++ Reflection/src/main/java/ru/skwardlow/reflection/Reflection.java
@@ -3,23 +3,26 @@ package ru.skwardlow.reflection;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Reflection {
 
         public static void main(String[] args) {
-            ArrayList<Double> list = new ArrayList<>();
+            LinkedList<Double> list = new LinkedList<>();
             Class arraysInfo = list.getClass();
 
-            System.out.println("Task 2: Java рефлексия");
             System.out.println("Название класса: " + arraysInfo.getName());
+
             Field[] fields = arraysInfo.getFields();
 
-            System.out.println("Поля класса:");
+            System.out.println("\nПоля класса:\n");
+
             for(Field field : fields) {
                 System.out.println(field.getName());
             }
 
-            System.out.println("Методы класса:");
+            System.out.println("\nМетоды класса:\n");
+
             Method[] methods = arraysInfo.getMethods();
             for(Method method : methods) {
                 System.out.println(method.getName());
